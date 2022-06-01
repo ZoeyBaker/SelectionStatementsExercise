96535a6b0e356f9b30d59ec8c351b5b4208c303a
Zoey Baker <zoeybaker625@gmail.com>
2022-06-01 07:50:18 -0500

    initial commit

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess the correct number!");
+            Console.WriteLine("What is the upper limit for the range of the number?");
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse);
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+            if (guess > number)
+            {
+
+                Console.WriteLine("Your guess is too high! Sorry");
+
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Your guess is too high! Sorry");
+            }
+            else
+            {
+
+                Console.WriteLine("Correct!");
+
+            }
+
+
+        }
+    }
+}
